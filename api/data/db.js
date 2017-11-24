4d1c483221424e537be268d4e20497e85166f67e
Johnny1109 <janakiraman.shanmugam@gmail.com>
2017-11-24 19:37:58 +0530

    Update db.js

--- api/data/db.js
+++ api/data/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var dburl = 'mongodb://johnny:test123@ec2-52-14-108-178.us-east-2.compute.amazonaws.com:27017/ctol';
+var dburl = 'mongodb://johnny:test123@ec2-13-127-45-76.ap-south-1.compute.amazonaws.com:27017/dummyDB';
 var retry = null;
 mongoose.connect(dburl);
 
